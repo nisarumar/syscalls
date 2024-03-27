21cc7cf1e1dba02e3840a9e61016f2dbeae7ec27
Umar <umarnisar@outlook.com>
2024-03-27 16:31:06 +0100

    soln to 09-xattr

--- 09-xattr/checksum.c
+++ 09-xattr/checksum.c
@@ -20,7 +20,12 @@ char * map_file(char *fn, ssize_t *len, int *fd) {
     // FIXME: Map file to memory
     // FIXME: Set *len = ...
     // FIXME: Set *fd = ...
-    return NULL;
+    void* addr = mmap(NULL, *len, PROT_READ|PROT_WRITE, MAP_SHARED, *fd, 0);
+
+    if ((uint64_t) addr < 0)
+	    return NULL;
+    else
+	    return (char *) addr;
 }
 
 // A (very) simple checksum function that calculates and additive checksum over a memory range.
@@ -50,23 +55,71 @@ int main(int argc, char *argv[]) {
     char *fn;
 
     // Should we reset the checksum?
-    bool reset_checksum;
+    bool reset_checksum = false;
 
     // Argument parsing
     if (argc == 3 && strcmp(argv[1], "-r") == 0) {
         reset_checksum = true;
         fn = argv[2];
     } else if (argc == 2) {
-        reset_checksum = true;
         fn = argv[1];
     } else {
         fprintf(stderr, "usage: %s [-r] <FILE>\n", argv[0]);
     }
     // Avoid compiler warnings
-    (void) fn; (void) reset_checksum; (void) xattr;
-    // FIXME: map the file
-    // FIXME: reset the checksum if requested
-    // FIXME: calculate the checksum
-    // FIXME: get the old checksum and perform checking
-    // FIXME: set the new checksum
+
+    struct stat s;
+    int rc = stat(fn, &s);
+    if (rc < 0)
+	    die("stat");
+
+    int fd = open(fn, O_RDWR);
+
+    if (fd < 0)
+	    die("open");
+
+    char* addr = map_file(fn, &s.st_size, &fd);
+    
+    if (addr == NULL)
+	    die("map");
+
+
+    if (reset_checksum)
+   {
+	    int rc = fremovexattr(fd,xattr);
+	    if (rc<0 && errno!=ENODATA)
+		    die("fremovexattr");
+
+	    return 0;
+    }
+
+
+    uint64_t checksum = calc_checksum(addr, s.st_size);
+    printf("checksum: %lx\n", checksum);
+
+    uint64_t value = 0;
+    ssize_t size = 0;
+    size = fgetxattr(fd, xattr, &value , sizeof(value));
+
+    if (size!=sizeof(value))
+    {
+	    printf("Null Checksum\n");
+    }
+    else
+    {
+	    printf("current checksum: %lx\n", value);
+	    if (value != checksum)
+		    fprintf(stderr, "checksum don't match\n");
+
+	    return 0;
+    }
+
+
+
+    int err = setxattr(fn, xattr, &checksum, sizeof(uint64_t), 0);
+
+    if (err<0)
+	    die("set checksum");
+
+    return 0;
 }
